72a49bea50fd375bec74f746e450ecd86c7bdbb4
pabloaGaitan <pabloagaitanb@hotmail.com>
2017-06-10 14:49:16 -0500

    arreglado lo de dependencias

--- src/main/java/com/knowshare/api/main/Main.java
+++ src/main/java/com/knowshare/api/main/Main.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.knowshare.enterprise.bean"})
+@ComponentScan(basePackages={"com.knowshare"})
 public class Main {
 	
 	public static void main(String[] args) {
