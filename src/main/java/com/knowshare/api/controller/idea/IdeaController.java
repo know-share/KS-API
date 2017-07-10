401b08b18ebcc747a88aeb6b893f2a9943378f56
Pablo <pablo.gaitan@javeriana.edu.co>
2017-07-09 23:36:37 -0500

    metodo crear idea devuelve la idea creada

--- src/main/java/com/knowshare/api/controller/idea/IdeaController.java
+++ src/main/java/com/knowshare/api/controller/idea/IdeaController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.knowshare.api.security.JWTFilter;
 import com.knowshare.dto.idea.IdeaDTO;
-import com.knowshare.dto.perfilusuario.UsuarioDTO;
 import com.knowshare.enterprise.bean.idea.IdeaFacade;
-import com.knowshare.enterprise.bean.usuario.UsuarioFacade;
 import com.knowshare.enterprise.repository.app.UserSessionRepository;
 import com.knowshare.entities.app.UserSession;
-
 /**
  * @author Pablo Gaitan
  *
@@ -38,25 +34,18 @@ public class IdeaController {
 	@Autowired
 	private UserSessionRepository userSessionRepository;
 	
-	@Autowired
-	private UsuarioFacade usuarioBean;
 	
-	@RequestMapping(value="/crear/{username:.+}" ,method = RequestMethod.POST)
-	public ResponseEntity<?> crearIdea(@PathVariable String username,
-			@RequestBody IdeaDTO idea,
+	@RequestMapping(value="/crear" ,method = RequestMethod.POST)
+	public ResponseEntity<?> crearIdea(@RequestBody IdeaDTO idea,
 			@RequestHeader("Authorization") String token){
 		UserSession sesion = userSessionRepository.findByToken(token);
-		if(!JWTFilter.validateToken(token, sesion.getSecretKey())){
+		if(sesion == null|| !JWTFilter.validateToken(token, sesion.getSecretKey())){
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
 		}
-		
-		if(!usuarioBean.isUsernameTaken(username)){
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-		}
-		UsuarioDTO usuario = usuarioBean.getUsuario(username);
-		idea.setUsuario(usuario);
-		if(ideaBean.crearIdea(idea) != null){
-			return ResponseEntity.status(HttpStatus.OK).body(null);
+		idea.setUsuario(sesion.getUsername());
+		IdeaDTO crear = ideaBean.crearIdea(idea);
+		if(crear!= null){
+			return ResponseEntity.status(HttpStatus.OK).body(crear);
 		}
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 		
