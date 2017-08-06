10c0a99c13b68e9bbd7ad9e6d3e75dc468159184
JFBF <felichiki19@hotmail.com>
2017-08-05 20:27:07 -0500

    Se agregó método para update en Carrera

--- src/main/java/com/knowshare/api/controller/academia/CarreraController.java
+++ src/main/java/com/knowshare/api/controller/academia/CarreraController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -52,6 +53,17 @@ public class CarreraController{
 					.body(null);
 		return ResponseEntity.status(HttpStatus.OK)
 				.body(dto);
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.PATCH)
+	public ResponseEntity<?> update (@RequestBody CarreraDTO carrera){
+		if(carrera != null){
+			if(carreraBean.update(carrera))
+				return ResponseEntity.status(HttpStatus.OK).body(null);
+			else
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
 	}
 
 }
