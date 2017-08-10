2bbc54ff8e7cbe24c934cc016f444833861990ca
JFBF <felichiki19@hotmail.com>
2017-08-09 21:45:56 -0500

    CRUD Tag completo

--- src/main/java/com/knowshare/api/controller/idea/TagController.java
+++ src/main/java/com/knowshare/api/controller/idea/TagController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,5 +32,36 @@ public class TagController {
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(tags);
+	}
+	
+	@RequestMapping(value="create", method=RequestMethod.POST)
+	public ResponseEntity<?> create (@RequestBody String tag){
+		if(tag != null){
+			if(tagBean.create(tag))
+				return ResponseEntity.status(HttpStatus.OK).body(null);
+			else
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.PATCH)
+	public ResponseEntity<?> update (@RequestBody Tag tag){
+		if(tag != null){
+			if(tagBean.update(tag))
+				return ResponseEntity.status(HttpStatus.OK).body(null);
+			else
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+	}
+	
+
+	@RequestMapping(value="delete/{id:.+}", method=RequestMethod.DELETE)
+	public ResponseEntity<?> delete (@PathVariable String id){
+		if(tagBean.delete(id)) {
+			return ResponseEntity.status(HttpStatus.OK).body(null); 
+		}
+		return ResponseEntity.status(HttpStatus.NOT_MODIFIED).body(null);
 	}
 }
